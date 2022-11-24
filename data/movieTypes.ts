5465971a2c8342cbb481c59f66c5c97682dfb059
[Soheil1400] <[soheil.saedi@ymail.com]>
2022-11-24 11:48:47 +0330

    second commit

---
+++ data/movieTypes.ts
@@ -0,0 +1,7 @@
+import {movieType} from "../model/movieType";
+
+export const movieTypes: movieType[] = [
+    {id:1,title:'انیمیشن'},
+    {id:2,title:'هیجان انگیز'},
+    {id:3,title:'ترسناک'},
+]
